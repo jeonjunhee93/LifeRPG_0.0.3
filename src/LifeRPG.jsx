300459487f5afae873c0cf4660b9589be4081967
jeonjunhee93 <lllllljz@naver.com>
2025-10-31 22:14:35 +0900

    Add files via upload

---
+++ src/LifeRPG.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default function LifeRPG() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Life RPG Game</h1>
+      <p>이곳에 캐릭터, 장비창, 퀘스트, 상점 기능 등 전체 로직이 포함됩니다.</p>
+      <img src="/assets/무딘칼_일반.png" alt="무기 아이콘" width="64" />
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LifeRPG from './LifeRPG';
+import '../index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LifeRPG />
+  </React.StrictMode>
+);
